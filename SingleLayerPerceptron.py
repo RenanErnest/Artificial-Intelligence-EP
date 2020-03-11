896d383d319b5c06e036701373119a12f5606ccd
RenanErnest <renan.erns@gmail.com>
2020-03-11 10:44:51 -0300

    Some tests and ideas

--- SingleLayerPerceptron.py
+++ SingleLayerPerceptron.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class SingleLayerPerceptron:
 
     def perceptron(self,inputs,weights,threshold):
@@ -7,3 +9,14 @@ class SingleLayerPerceptron:
         if summ > threshold:
             return 1
         return 0
+
+    def updateWeights(self,inputs,weights,desired,learning_rate):
+        new_weights = [0] * len(weights)
+        for i in range(len(weights)):
+            new_weights[i] = weights[i] + learning_rate*desired*inputs[i] # Fausett
+            # new_weights[i] = weights[i] + (desired-output)*inputs[i] #another aproach
+        return new_weights
+
+    data = pd.read_csv('Data/problemAND.csv')
+    print(data.head())
+
